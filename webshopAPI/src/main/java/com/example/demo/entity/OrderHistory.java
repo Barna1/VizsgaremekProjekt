a37686fe9b4f1d35052638fffe7e594bccff323c
Barna1 <barnabas121744@gmail.com>
2026-01-06 19:39:38 +0100

    Entity classok annotációi

--- webshopAPI/src/main/java/com/example/demo/entity/OrderHistory.java
+++ webshopAPI/src/main/java/com/example/demo/entity/OrderHistory.java
@@ -1,4 +1,17 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "order_history")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class OrderHistory {
 }
