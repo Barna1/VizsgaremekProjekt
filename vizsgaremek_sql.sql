bd53c8bf6e1ba6fad48fde8e3c21c62cb1a310eb
Barna1 <barnabas121744@gmail.com>
2026-01-07 18:03:21 +0100

    Update vizsgaremek_sql.sql

--- vizsgaremek_sql.sql
+++ vizsgaremek_sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: localhost:3306
--- Létrehozás ideje: 2026. Jan 07. 17:01
+-- Létrehozás ideje: 2026. Jan 07. 17:03
 -- Kiszolgáló verziója: 5.7.24
 -- PHP verzió: 8.3.1
 
@@ -206,6 +206,12 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `getOrderHistoryById` (IN `idIN` INT
 	SELECT * FROM order_history
     WHERE 
     order_history.id = idIN;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getOrderHistoryByUserId` (IN `userIdIN` INT)   BEGIN 
+	SELECT * FROM order_history
+    WHERE 
+    order_history.user_id = userIdIN;
 END$$
 
 DELIMITER ;
