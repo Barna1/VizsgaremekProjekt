5bee6c5a5a8db260107a9003f1e418244c0bf195
Barna1 <barnabas121744@gmail.com>
2026-01-09 18:20:44 +0100

    Login endpoint megírása

--- webshopAPI/src/main/java/com/example/demo/controller/UserController.java
+++ webshopAPI/src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.UserService;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/login")
+    private ResponseEntity<Object> login(@RequestBody JsonNode requestBody) {
+        return userService.login(requestBody.get("username").asText(null), requestBody.get("password").asText(null));
+    }
 }
