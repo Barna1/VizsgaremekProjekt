1a128752a988cb21f3187e962889552d44980590
Barna1 <barnabas121744@gmail.com>
2026-01-24 09:09:09 +0100

    Email validáció

--- webshopAPI/src/main/java/com/example/demo/service/UserService.java
+++ webshopAPI/src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.config.email.EmailSender;
 import com.example.demo.entity.Basket;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
@@ -9,12 +10,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final EmailSender emailSender;
 
     public ResponseEntity<Object> login(String username, String password) {
         try {
@@ -91,5 +94,14 @@ public class UserService {
             e.printStackTrace();
             return ResponseEntity.internalServerError().build();
         }
+    }
+
+
+    public Boolean isEmailValid(String email) {
+        Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+        if (email == null || email.length() > 100) {
+            return false;
+        }
+        return EMAIL_PATTERN.matcher(email).matches();
     }
 }
