93001b16dc4cbbe66b40ff3b0cb620fffcef68ed
Barna1 <barnabas121744@gmail.com>
2026-01-24 13:14:20 +0100

    Címtípusok lekérdezése

--- webshopAPI/src/main/java/com/example/demo/service/OtherService.java
+++ webshopAPI/src/main/java/com/example/demo/service/OtherService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.AddressTypeRepository;
 import com.example.demo.repository.PaymentMethodRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +11,19 @@ import org.springframework.stereotype.Service;
 public class OtherService {
 
     private final PaymentMethodRepository paymentMethodRepository;
+    private final AddressTypeRepository addressTypeRepository;
 
     public ResponseEntity<Object> getAllPaymentMethod() {
         try {
             return ResponseEntity.ok().body(paymentMethodRepository.findAll());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+    public ResponseEntity<Object> getAllAddressType() {
+        try {
+            return ResponseEntity.ok().body(addressTypeRepository.findAll());
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.internalServerError().build();
