d8e1f4cba0901142053e44ed04c79f5a0d0f8c91
Barna1 <barnabas121744@gmail.com>
2026-01-24 12:48:42 +0100

    Fizetési módok lekérdezése

--- webshopAPI/src/main/java/com/example/demo/service/OtherService.java
+++ webshopAPI/src/main/java/com/example/demo/service/OtherService.java
@@ -1,4 +1,22 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.PaymentMethodRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class OtherService {
+
+    private final PaymentMethodRepository paymentMethodRepository;
+
+    public ResponseEntity<Object> getAllPaymentMethod() {
+        try {
+            return ResponseEntity.ok().body(paymentMethodRepository.findAll());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
