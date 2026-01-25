e4fead41f515dd25275b2769df868b6d0d5b4488
Barna1 <barnabas121744@gmail.com>
2026-01-25 19:29:53 +0100

    BookRepository megírása

--- webshopAPI/src/main/java/com/example/demo/repository/BookRepository.java
+++ webshopAPI/src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,7 +1,27 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    @Procedure(name = "getBookById", procedureName = "getBookById")
+    Optional<Book> getBookById(@Param("idIN") Integer id);
+
+    @Procedure(name = "getMostSuccessfullyBooks", procedureName = "getMostSuccessfullyBooks")
+    List<Book> getMostSuccessfullyBooks();
+
+    @Procedure(name = "deleteBookById", procedureName = "deleteBookById")
+    void deleteBookById(@Param("idIN") Integer id);
+
+    @Procedure(name = "getAllBookIdByAuthor", procedureName = "getAllBookIdByAuthor")
+    List<Integer> getAllBookIdByAuthor(@Param("authorIdIN") Integer authorId);
+
+    Page<Book> findByIsDeleted(Boolean isDeleted, Pageable pageable);
 }
