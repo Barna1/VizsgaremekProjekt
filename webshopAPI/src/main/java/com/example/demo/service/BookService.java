17051520753fe09358a7ba66da10e93ac76954f4
Barna1 <barnabas121744@gmail.com>
2026-01-25 20:04:39 +0100

    Könyvek lekérése a service-en

--- webshopAPI/src/main/java/com/example/demo/service/BookService.java
+++ webshopAPI/src/main/java/com/example/demo/service/BookService.java
@@ -1,4 +1,29 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BookService {
+    private final BookRepository bookRepository;
+    public ResponseEntity<Object> getBooks(Pageable pageable) {
+        try {
+            Page<Book> returnList = bookRepository.findByIsDeleted(false, pageable);
+            HttpHeaders header = new HttpHeaders();
+            header.add("TotalPage", returnList.getTotalPages()+"");
+
+            return new ResponseEntity<>(returnList.toList(), header, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
