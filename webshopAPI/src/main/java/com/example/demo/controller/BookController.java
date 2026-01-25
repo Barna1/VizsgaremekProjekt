7865db797f5d689902db5db7b7def51e397475f4
Barna1 <barnabas121744@gmail.com>
2026-01-25 20:10:36 +0100

    Könyvek lekérése endpoint

--- webshopAPI/src/main/java/com/example/demo/controller/BookController.java
+++ webshopAPI/src/main/java/com/example/demo/controller/BookController.java
@@ -1,4 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class BookController {
+    private final BookService bookService;
+    
+    @GetMapping("")
+    private ResponseEntity<Object> getBooks(Pageable pageable) {
+        return bookService.getBooks(pageable);
+    }
 }
