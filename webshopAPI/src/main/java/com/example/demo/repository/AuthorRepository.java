bc376dfd05907d4d936670fea3bc9ff2244a8e74
Barna1 <barnabas121744@gmail.com>
2026-01-26 12:40:48 +0100

    AuthorRepository megírása

--- webshopAPI/src/main/java/com/example/demo/repository/AuthorRepository.java
+++ webshopAPI/src/main/java/com/example/demo/repository/AuthorRepository.java
@@ -2,6 +2,19 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    @Procedure(name = "getAllAuthor", procedureName = "getAllAuthor")
+    List<Author> getAllAuthor();
+
+    @Procedure(name = "getAuthorById", procedureName = "getAuthorById")
+    Optional<Author> getAuthorById(@Param("idIN") Integer id);
+
+    @Procedure(name = "deleteAuthor", procedureName = "deleteAuthor")
+    void deleteAuthor(@Param("idIN") Integer id);
 }
