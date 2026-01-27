7465c33c95224d4d50f4c6fadff9176b2de55332
Barna1 <barnabas121744@gmail.com>
2026-01-27 18:12:20 +0100

    BasketRepository megírása

--- webshopAPI/src/main/java/com/example/demo/repository/BasketRepository.java
+++ webshopAPI/src/main/java/com/example/demo/repository/BasketRepository.java
@@ -2,6 +2,18 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Basket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BasketRepository extends JpaRepository<Basket, Integer> {
+    @Procedure(name = "getBasketByUserId", procedureName = "getBasketByUserId")
+    Optional<Basket> getBasketByUserId(@Param("userIdIN") Integer userId);
+
+    @Procedure(name = "getBasketById", procedureName = "getBasketById")
+    Optional<Basket> getBasketById(@Param("idIN") Integer basketId);
+
+    @Procedure(name = "clearBasket", procedureName = "clearBasket")
+    void clearBasket(@Param("idIN") Integer basketId);
 }
