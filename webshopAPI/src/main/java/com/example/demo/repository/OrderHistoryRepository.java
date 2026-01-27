1de1faecaad7d6a83a1cb33f565b05d8d638cfe0
Barna1 <barnabas121744@gmail.com>
2026-01-27 19:14:04 +0100

    OrderHistoryRepository megírása

--- webshopAPI/src/main/java/com/example/demo/repository/OrderHistoryRepository.java
+++ webshopAPI/src/main/java/com/example/demo/repository/OrderHistoryRepository.java
@@ -2,6 +2,19 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.OrderHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Integer> {
+    @Procedure(name = "getOrderHistoryByUserId", procedureName = "getOrderHistoryByUserId")
+    List<OrderHistory> getOrderHistoryByUserId(@Param("userIdIN") Integer userId);
+
+    @Procedure(name = "getOrderHistoriesByEmail", procedureName = "getOrderHistoriesByEmail")
+    List<OrderHistory> getOrderHistoriesByEmail(@Param("emailIN") String email);
+
+    @Procedure(name = "getOrderHistoryById", procedureName = "getOrderHistoryById")
+    Optional<OrderHistory> getOrderHistoryById(@Param("idIN") Integer id);
 }
