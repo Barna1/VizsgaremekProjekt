7a775df2428a568ad1be19538c79fae963ff4168
Barna1 <barnabas121744@gmail.com>
2026-01-27 18:45:13 +0100

    BasketProductRepository megírása

--- webshopAPI/src/main/java/com/example/demo/repository/BasketProductRepository.java
+++ webshopAPI/src/main/java/com/example/demo/repository/BasketProductRepository.java
@@ -2,6 +2,15 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.BasketProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BasketProductRepository extends JpaRepository<BasketProduct, Integer> {
+    @Procedure(name = "getBasketProductById", procedureName = "getBasketProductById")
+    Optional<BasketProduct> getBasketProductById(@Param("idIN") Integer id);
+
+    @Procedure(name = "deleteProductFromBasket", procedureName = "deleteProductFromBasket")
+    void deleteProductFromBasket(@Param("idIN") Integer id);
 }
