cd09ca41273c9ef8df2fb126e407259c6906b18d
Barna1 <barnabas121744@gmail.com>
2026-01-27 17:43:57 +0100

    AddressTypeRepository megírása

--- webshopAPI/src/main/java/com/example/demo/repository/AddressTypeRepository.java
+++ webshopAPI/src/main/java/com/example/demo/repository/AddressTypeRepository.java
@@ -2,6 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.AddressType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AddressTypeRepository extends JpaRepository<AddressType, Integer> {
+    @Procedure(name = "getAddressTypeById", procedureName = "getAddressTypeById")
+    Optional<AddressType> getAddressTypeById(@Param("idIN") Integer id);
 }
