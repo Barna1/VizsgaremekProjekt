a78637503d6062a2eec9e5c7f3633d9cfc1621b5
Barna1 <barnabas121744@gmail.com>
2026-01-27 09:29:28 +0100

    GenreRepository megírása

--- webshopAPI/src/main/java/com/example/demo/repository/GenreRepository.java
+++ webshopAPI/src/main/java/com/example/demo/repository/GenreRepository.java
@@ -2,6 +2,19 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
+    @Procedure(name = "getAllGenre", procedureName = "getAllGenre")
+    List<Genre> getAllGenre();
+
+    @Procedure(name = "deleteGenre", procedureName = "deleteGenre")
+    void deleteGenre(@Param("idIN") Integer id);
+
+    @Procedure(name = "getGenreById", procedureName = "getGenreById")
+    Optional<Genre> getGenreById(@Param("idIN") Integer id);
 }
