f016c5d85e4781bb1e0874ca824bbbef0744dbaf
Barna1 <barnabas121744@gmail.com>
2026-01-27 12:28:28 +0100

    PublisherRepository megírása

--- webshopAPI/src/main/java/com/example/demo/repository/PublisherRepository.java
+++ webshopAPI/src/main/java/com/example/demo/repository/PublisherRepository.java
@@ -2,6 +2,19 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
+    @Procedure(name = "getPublisherById", procedureName = "getPublisherById")
+    Optional<Publisher> getPublisherById(@Param("idIN") Integer id);
+
+    @Procedure(name = "getAllPublisher", procedureName = "getAllPublisher")
+    List<Publisher> getAllPublisher();
+
+    @Procedure(name = "deletePublisher", procedureName = "deletePublisher")
+    void deletePublisher(@Param("idIN") Integer id);
 }
