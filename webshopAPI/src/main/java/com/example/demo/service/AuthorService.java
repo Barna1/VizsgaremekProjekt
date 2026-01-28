6d55cade941ed68ea05266c545180bfa54b2f435
Barna1 <barnabas121744@gmail.com>
2026-01-28 18:15:19 +0100

    Összes szerző megírása

--- webshopAPI/src/main/java/com/example/demo/service/AuthorService.java
+++ webshopAPI/src/main/java/com/example/demo/service/AuthorService.java
@@ -1,4 +1,29 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.ConstraintViolationException;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(noRollbackFor = {DataIntegrityViolationException.class, ConstraintViolationException.class, SQLIntegrityConstraintViolationException.class, SQLException.class})
+
 public class AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public ResponseEntity<Object> getAllAuthor() {
+        try {
+            return ResponseEntity.ok().body(authorRepository.getAllAuthor());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
