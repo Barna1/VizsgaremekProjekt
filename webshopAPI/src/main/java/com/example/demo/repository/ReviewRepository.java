cde1de61d5d41e807c0bc0757cbf27fb1a08df3d
Barna1 <barnabas121744@gmail.com>
2026-01-28 10:14:20 +0100

    ReviewRepository megírása

--- webshopAPI/src/main/java/com/example/demo/repository/ReviewRepository.java
+++ webshopAPI/src/main/java/com/example/demo/repository/ReviewRepository.java
@@ -2,6 +2,15 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    @Procedure(name = "deleteReviewById", procedureName = "deleteReviewById")
+    void deleteReviewById(@Param("idIN") Integer id);
+
+    @Procedure(name = "getReviewById", procedureName = "getReviewById")
+    Optional<Review> getReviewById(@Param("idIN") Integer id);
 }
