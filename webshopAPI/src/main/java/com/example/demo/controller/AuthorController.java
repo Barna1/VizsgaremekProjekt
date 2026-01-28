30f9a52c675a461e86926bb9d19696072a6bc8f7
Barna1 <barnabas121744@gmail.com>
2026-01-28 18:42:52 +0100

    Összes szerző endpoint

--- webshopAPI/src/main/java/com/example/demo/controller/AuthorController.java
+++ webshopAPI/src/main/java/com/example/demo/controller/AuthorController.java
@@ -1,4 +1,20 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.AuthorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/author")
+@RequiredArgsConstructor
 public class AuthorController {
+    private final AuthorService authorService;
+
+    @GetMapping("")
+    private ResponseEntity<Object> getAllAuthor() {
+        return authorService.getAllAuthor();
+    }
 }
