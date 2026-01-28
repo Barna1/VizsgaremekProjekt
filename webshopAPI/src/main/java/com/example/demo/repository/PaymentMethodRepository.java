dd478cdba19af5232ee3338f7178733a5464d179
Barna1 <barnabas121744@gmail.com>
2026-01-28 09:43:22 +0100

    PaymentMethodRepository megírása

--- webshopAPI/src/main/java/com/example/demo/repository/PaymentMethodRepository.java
+++ webshopAPI/src/main/java/com/example/demo/repository/PaymentMethodRepository.java
@@ -2,6 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.PaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Integer> {
+    @Procedure(name = "getPaymentMethodById", procedureName = "getPaymentMethodById")
+    Optional<PaymentMethod> getPaymentMethodById(@Param("idIN") Integer id);
 }
