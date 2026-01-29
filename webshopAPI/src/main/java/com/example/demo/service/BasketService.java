d89a18a48375bfdc313204581f7cb7db722cd790
Barna1 <barnabas121744@gmail.com>
2026-01-29 13:28:30 +0100

    Elem törlése a kosárból

--- webshopAPI/src/main/java/com/example/demo/service/BasketService.java
+++ webshopAPI/src/main/java/com/example/demo/service/BasketService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Basket;
+import com.example.demo.entity.BasketProduct;
 import com.example.demo.entity.User;
+import com.example.demo.repository.BasketProductRepository;
 import com.example.demo.repository.BasketRepository;
+import com.example.demo.repository.BookRepository;
 import com.example.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -20,6 +23,8 @@ import java.sql.SQLIntegrityConstraintViolationException;
 public class BasketService {
     private final UserRepository userRepository;
     private final BasketRepository basketRepository;
+    private final BasketProductRepository basketProductRepository;
+    private final BookRepository bookRepository;
 
     public ResponseEntity<Object> getBasketByUserId(Integer userId) {
         try {
@@ -35,6 +40,31 @@ public class BasketService {
             Basket basket = basketRepository.getBasketByUserId(userId).orElse(null);
             basket.setProductList(basket.getProductList().stream().filter(product -> !product.getIsDeleted()).toList());
             return ResponseEntity.ok().body(basket);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+    public ResponseEntity<Object> deleteProductFromBasket(Integer basketProductId, Integer basketId) {
+        try {
+            if (basketProductId == null || basketId == null) {
+                return ResponseEntity.status(422).build();
+            }
+
+            Basket searchedBasket = basketRepository.getBasketById(basketId).orElse(null);
+            if (searchedBasket == null) {
+                return ResponseEntity.status(404).body("basketNotFound");
+            }
+            BasketProduct searchedProduct = basketProductRepository.getBasketProductById(basketProductId).orElse(null);
+            if (searchedProduct == null || searchedProduct.getIsDeleted()) {
+                return ResponseEntity.status(404).body("productNotFound");
+            }
+
+            basketProductRepository.deleteProductFromBasket(searchedProduct.getId());
+            searchedProduct.getBasketBook().setStockQuantity(searchedProduct.getBasketBook().getStockQuantity() + searchedProduct.getAmount());
+            bookRepository.save(searchedProduct.getBasketBook());
+
+            return ResponseEntity.ok().build();
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.internalServerError().build();
