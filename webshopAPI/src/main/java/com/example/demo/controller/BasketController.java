d9eb1eefe74a086cf68fb65d174113123ef39002
Barna1 <barnabas121744@gmail.com>
2026-01-29 12:52:37 +0100

    Kosár lekérése userId endpoint

--- webshopAPI/src/main/java/com/example/demo/controller/BasketController.java
+++ webshopAPI/src/main/java/com/example/demo/controller/BasketController.java
@@ -1,4 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.BasketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/basket")
 public class BasketController {
+    private final BasketService basketService;
+
+    @GetMapping("/user/{id}")
+    private ResponseEntity<Object> getBasketByUserId(@PathVariable("id") Integer userId) {
+        return basketService.getBasketByUserId(userId);
+    }
 }
