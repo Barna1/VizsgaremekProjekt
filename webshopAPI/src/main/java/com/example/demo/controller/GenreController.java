95c6ccb797620e9957245a877577416cf045ca94
Barna1 <barnabas121744@gmail.com>
2026-01-30 08:45:04 +0100

    Összes műfaj lekérése endpoint

--- webshopAPI/src/main/java/com/example/demo/controller/GenreController.java
+++ webshopAPI/src/main/java/com/example/demo/controller/GenreController.java
@@ -1,4 +1,20 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.GenreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/genre")
 public class GenreController {
+    private final GenreService genreService;
+
+    @GetMapping("")
+    private ResponseEntity<Object> getAllGenre() {
+        return genreService.getAllGenre();
+    }
 }
