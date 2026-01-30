3f33d4d6d5b824b64532b043232197bffae2c130
Barna1 <barnabas121744@gmail.com>
2026-01-30 08:32:03 +0100

    Összes műfaj lekérése

--- webshopAPI/src/main/java/com/example/demo/service/GenreService.java
+++ webshopAPI/src/main/java/com/example/demo/service/GenreService.java
@@ -1,4 +1,28 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.GenreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.ConstraintViolationException;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(noRollbackFor = {DataIntegrityViolationException.class, ConstraintViolationException.class, SQLIntegrityConstraintViolationException.class, SQLException.class})
 public class GenreService {
+    private final GenreRepository genreRepository;
+
+    public ResponseEntity<Object> getAllGenre() {
+        try {
+            return ResponseEntity.ok().body(genreRepository.getAllGenre());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
