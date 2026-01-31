a9702a8e412a5a1c7bbd88d38ea97b10dc9f21e0
Barna1 <barnabas121744@gmail.com>
2026-01-31 09:30:07 +0100

    OrderHistory lekérése userId endpoint

--- webshopAPI/src/main/java/com/example/demo/controller/OrderController.java
+++ webshopAPI/src/main/java/com/example/demo/controller/OrderController.java
@@ -1,4 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/order")
+
 public class OrderController {
+    private final OrderService orderService;
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Object> getOrderHistoryByUserId(@PathVariable("id") Integer userId) {
+        return orderService.getOrderHistoryByUserId(userId);
+    }
 }
