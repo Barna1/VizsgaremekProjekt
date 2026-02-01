68964de92f36489ce4099aea6b659dbb08307521
Barna1 <barnabas121744@gmail.com>
2026-02-01 16:50:21 +0100

    Rendelés küldése

--- webshopAPI/src/main/java/com/example/demo/service/OrderService.java
+++ webshopAPI/src/main/java/com/example/demo/service/OrderService.java
@@ -1,8 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.config.email.EmailSender;
-import com.example.demo.entity.OrderHistory;
-import com.example.demo.entity.User;
+import com.example.demo.entity.*;
+import com.example.demo.repository.BookRepository;
 import com.example.demo.repository.OrderHistoryRepository;
 import com.example.demo.repository.StatusRepository;
 import com.example.demo.repository.UserRepository;
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.validation.ConstraintViolationException;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -28,6 +29,7 @@ public class OrderService {
     private final StatusRepository statusRepository;
     private final EmailSender emailSender;
     private final PasswordEncoder passwordEncoder;
+    private final BookRepository bookRepository;
 
     public ResponseEntity<Object> getOrderHistoryByUserId(Integer userId) {
         try {
@@ -101,6 +103,71 @@ public class OrderService {
     public ResponseEntity<Object> getAllOrder() {
         try {
             return ResponseEntity.ok().body(orderHistoryRepository.findAll());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+    public ResponseEntity<Object> sendOrder(OrderHistory newOrder, Integer basketId) {
+        try {
+            if (newOrder == null || basketId == null) {
+                return ResponseEntity.status(422).build();
+            }
+
+            if (newOrder.getOrdererUser() != null) {
+                User searchedUser = userRepository.getUserById(newOrder.getOrdererUser().getId()).orElse(null);
+                if (searchedUser == null || searchedUser.getIsDeleted()) {
+                    return ResponseEntity.status(404).body("userNotFound");
+                }
+            }
+
+            PaymentMethod searchedPaymentMethod = paymentMethodRepository.getPaymentMethodById(newOrder.getPaymentMethod().getId()).orElse(null);
+            Basket searchedBasket = basketRepository.getBasketById(basketId).orElse(null);
+
+            if (searchedPaymentMethod == null) {
+                return ResponseEntity.status(404).body("paymentMethodNotFound");
+            } else if (searchedBasket == null) {
+                return ResponseEntity.status(404).body("basketNotFound");
+            }
+
+            if (newOrder.getId() != null) {
+                return ResponseEntity.status(415).body("invalidObject");
+            } else if (!isEmailValid(newOrder.getEmail().trim())) {
+                return ResponseEntity.status(415).body("invalidEmail");
+            } else if (!isPhoneValid(newOrder.getPhone())) {
+                return ResponseEntity.status(415).body("invalidPhone");
+            } else if (!isBillingDetailValid(newOrder.getHistoryBillingDetail())) {
+                return ResponseEntity.status(415).body("invalidBillingDetails");
+            } else if (!isTransportDetailValid(newOrder.getHistoryTransportDetail())) {
+                return ResponseEntity.status(415).body("invalidBillingDetails");
+            }
+
+            int sumPrice = 0;
+            List<OrderHistoryProduct> orderedProductList = new ArrayList<>();
+            for (int i = 0; i < searchedBasket.getProductList().size(); i++) {
+                BasketProduct productFromBasket = searchedBasket.getProductList().get(i);
+                Book book = productFromBasket.getBasketBook();
+                book.setStockQuantity(book.getStockQuantity() - productFromBasket.getAmount());
+
+                orderedProductList.add(new OrderHistoryProduct(productFromBasket.getAmount(), book));
+                bookRepository.save(book);
+                sumPrice += (book.getPrice() * productFromBasket.getAmount());
+            }
+
+            try {
+                emailSender.sendEmailAboutOrderWithVCode(newOrder.getEmail(), generateVCode());
+            } catch (Exception e) {
+                e.printStackTrace();
+                return ResponseEntity.internalServerError().body("emailSenderError");
+            }
+
+            System.out.println(sumPrice);
+
+            newOrder.setOrderHistoryProductList(orderedProductList);
+            newOrder.setStatus(statusRepository.findById(1).get());
+            orderHistoryRepository.save(newOrder);
+
+            return ResponseEntity.ok().build();
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.internalServerError().build();
