7553e7fb3991a7d0962c9c06451515010d81e5a3
Barna1 <barnabas121744@gmail.com>
2026-02-02 09:52:32 +0100

    Minden kiadó lekérése

--- webshopAPI/src/main/java/com/example/demo/service/PublisherService.java
+++ webshopAPI/src/main/java/com/example/demo/service/PublisherService.java
@@ -1,4 +1,27 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.PublisherRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.ConstraintViolationException;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(noRollbackFor = {DataIntegrityViolationException.class, ConstraintViolationException.class, SQLIntegrityConstraintViolationException.class, SQLException.class})
 public class PublisherService {
+    private final PublisherRepository publisherRepository;
+    public ResponseEntity<Object> getAllPublisher() {
+        try {
+            return ResponseEntity.ok().body(publisherRepository.getAllPublisher());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
