655634fca64fc351b92cca2069d1e9d7b132d8ba
Barna1 <barnabas121744@gmail.com>
2026-02-02 09:59:59 +0100

    Minden kiadó lekérése endpoint

--- webshopAPI/src/main/java/com/example/demo/controller/PublisherController.java
+++ webshopAPI/src/main/java/com/example/demo/controller/PublisherController.java
@@ -1,4 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.PublisherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/publisher")
+@RequiredArgsConstructor
 public class PublisherController {
+
+    private final PublisherService publisherService;
+
+    @GetMapping("")
+    private ResponseEntity<Object> getAllPublisher() {
+        return publisherService.getAllPublisher();
+    }
 }
