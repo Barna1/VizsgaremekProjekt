0d5a19cdb198202b96c27350e0769105e0e1eb1e
Barna1 <barnabas121744@gmail.com>
2026-02-02 11:00:08 +0100

    Vélemény hozzáadása endpoint

--- webshopAPI/src/main/java/com/example/demo/controller/ReviewController.java
+++ webshopAPI/src/main/java/com/example/demo/controller/ReviewController.java
@@ -1,4 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Review;
+import com.example.demo.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/review")
+@RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("")
+    private ResponseEntity<Object> addReview(@RequestBody Review newReview) {
+        return reviewService.addReview(newReview);
+    }
 }
