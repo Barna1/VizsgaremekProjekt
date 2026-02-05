67f92d03e6b76cbfa13aedd5ad54e0f387cae5e8
Barna1 <barnabas121744@gmail.com>
2026-02-05 12:29:53 +0100

    EmailSender megírása

--- webshopAPI/src/main/java/com/example/demo/config/email/EmailSender.java
+++ webshopAPI/src/main/java/com/example/demo/config/email/EmailSender.java
@@ -1,11 +1,23 @@
 package com.example.demo.config.email;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class EmailSender {
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine thymeleafTemplateEngine;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendVCodeForPasswordReset(String toEmail, String vCode){
 
     }
@@ -20,5 +32,11 @@ public class EmailSender {
     }
     public void sendEmailAboutOrderWithVCode(String toEmail, String vCode) {
 
+    }
+    private String getHtmlBody(String nameOfHtml, Map<String, Object> templateModel) {
+        Context thymeleafContext = new Context();
+        thymeleafContext.setVariables(templateModel);
+        String htmlBody = thymeleafTemplateEngine.process(nameOfHtml, thymeleafContext);
+        return htmlBody;
     }
 }
