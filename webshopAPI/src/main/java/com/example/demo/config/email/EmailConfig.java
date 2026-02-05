6c3cb5512d2561a9a763bfa894b8ac460b743a27
Barna1 <barnabas121744@gmail.com>
2026-02-05 12:22:00 +0100

    EmailConfig megírása

--- webshopAPI/src/main/java/com/example/demo/config/email/EmailConfig.java
+++ webshopAPI/src/main/java/com/example/demo/config/email/EmailConfig.java
@@ -1,8 +1,39 @@
 package com.example.demo.config.email;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
+//@PropertySource("classpath:application.properties")
 public class EmailConfig {
 
+    @Value("${spring.mail.username}")
+    private String emailAddress;
+
+    @Value("${spring.mail.password}")
+    private String emailPassword;
+
+    @Bean
+    JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(emailAddress);
+        mailSender.setPassword(emailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+
+        return mailSender;
+    }
 }
