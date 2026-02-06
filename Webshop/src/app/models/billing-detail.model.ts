d83e70d4ba65cba9f2b3e9f0fe8ec10e166e3d24
hauszmanndavid <hauszmannd@gmail.com>
2026-02-06 17:56:07 +0100

    billingDetail model megírása

--- Webshop/src/app/models/billing-detail.model.ts
+++ Webshop/src/app/models/billing-detail.model.ts
@@ -1,3 +1,15 @@
+import { AddressType } from "./address-type.model";
+
 export class BillingDetail {
-    
+  constructor(
+    public id: number,
+    public postCode: number,
+    public town: string,
+    public address: string,
+    public houseNumber: number,
+    public companyName: string | null = null,
+    public companyTaxNumber: string | null = null,
+    public other: string,
+    public billingDetailsAddressType: AddressType,
+  ) {}
 }
