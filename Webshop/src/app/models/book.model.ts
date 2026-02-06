e0a9e83419fb358a272eb1a733a8171b032012ff
hauszmanndavid <hauszmannd@gmail.com>
2026-02-06 19:06:54 +0100

    book model megírása

--- Webshop/src/app/models/book.model.ts
+++ Webshop/src/app/models/book.model.ts
@@ -1,2 +1,22 @@
+import { Author } from "./author.model";
+import { Genre } from "./genre.model";
+import { Publisher } from "./publisher.model";
+import { Review } from "./review.model";
+
 export class Book {
+  constructor(
+    public id: number,
+    public title: string,
+    public description: string,
+    public coverImgPath: string,
+    public publishingYear: number,
+    public ISBN: string,
+    public price: number,
+    public stockQuantity: number,
+    public publisher: Publisher,
+    public genreList: Genre[],
+    public reviewList: Review[],
+    public authors: Author[]
+  ) { }
 }
+
