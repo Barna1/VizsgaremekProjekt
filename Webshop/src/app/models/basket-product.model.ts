7628787c4f49d19a3cffc2cac388576744e242a3
hauszmanndavid <hauszmannd@gmail.com>
2026-02-06 14:35:08 +0100

    basketProduct megírása

--- Webshop/src/app/models/basket-product.model.ts
+++ Webshop/src/app/models/basket-product.model.ts
@@ -1,2 +1,7 @@
 export class BasketProduct {
+    constructor(
+        public id: number,
+        public amount: number,
+        public basketBook: Book
+    ) { }
 }
--- Webshop/src/app/models/basket.model.ts
+++ Webshop/src/app/models/basket.model.ts
@@ -1,7 +1,9 @@
+import { BasketProduct } from "./basket-product.model";
+
 export class Basket {
     constructor(
         public id: number | null,
         public totalPrice: number,
-       // public productList?: BasketProduct[]
+        public productList?: BasketProduct[]
     ) { }
 }
