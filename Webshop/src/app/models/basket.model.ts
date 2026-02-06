51e18ee0ac4d9b370bd0f459f13ee6601b53984e
hauszmanndavid <hauszmannd@gmail.com>
2026-02-06 13:46:52 +0100

    basket model megírása

--- Webshop/src/app/models/basket.model.ts
+++ Webshop/src/app/models/basket.model.ts
@@ -1,2 +1,7 @@
-export class BasketModel {
+export class Basket {
+    constructor(
+        public id: number | null,
+        public totalPrice: number,
+       // public productList?: BasketProduct[]
+    ) { }
 }
