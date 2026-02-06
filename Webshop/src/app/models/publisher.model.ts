abeb691475e5ed3dacf46bdff32700beb33d0de5
hauszmanndavid <hauszmannd@gmail.com>
2026-02-06 20:39:18 +0100

    publisher model megírása

--- Webshop/src/app/models/publisher.model.ts
+++ Webshop/src/app/models/publisher.model.ts
@@ -1,2 +1,12 @@
+import { Book } from "./book.model";
+
 export class Publisher {
+  constructor(
+    public id:number,
+    public name:string,
+    public email:string,
+    public phone:string,
+    public isbnSign:string,
+    public bookList:Book[]
+  ) { }
 }
