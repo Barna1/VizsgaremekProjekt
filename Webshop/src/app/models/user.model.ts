3830d66d854a0f822444866a4f60dbdf01ecc487
hauszmanndavid <hauszmannd@gmail.com>
2026-02-07 15:33:57 +0100

    user model megírása

--- Webshop/src/app/models/user.model.ts
+++ Webshop/src/app/models/user.model.ts
@@ -1,2 +1,14 @@
+import { Basket } from "./basket.model";
+import { Role } from "./role.model";
+
 export class User {
+  constructor(
+    public id: number | null,
+    public username: string,
+    public email: string,
+    public password: string,
+    public pfpPath?: string,
+    public role?: Role,
+    public basket?: Basket
+  ) { }
 }
