5761e028c50c140d9951e70b7c8a5ebba90f1ddf
hauszmanndavid <hauszmannd@gmail.com>
2026-02-08 10:23:35 +0100

    orderHistoryProduct model megírása

--- Webshop/src/app/models/order-history-product.model.ts
+++ Webshop/src/app/models/order-history-product.model.ts
@@ -1,2 +1,9 @@
+import { Book } from "./book.model";
+
 export class OrderHistoryProduct {
+  constructor(
+    public id: number,
+    public amount: number,
+    public orderHistoryBook: Book
+  ) { }
 }
