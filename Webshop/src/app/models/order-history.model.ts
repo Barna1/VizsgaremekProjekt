e918604b3f5814e48a5497e01291e346ca7ab900
hauszmanndavid <hauszmannd@gmail.com>
2026-02-08 11:53:10 +0100

    orderHistoy model megírása

--- Webshop/src/app/models/order-history.model.ts
+++ Webshop/src/app/models/order-history.model.ts
@@ -1,2 +1,25 @@
+import { BillingDetail } from "./billing-detail.model";
+import { OrderHistoryProduct } from "./order-history-product.model";
+import { PaymentMethod } from "./payment-method.model";
+import { Status } from "./status.model";
+import { TransportDetail } from "./transport-detail.model";
+import { User } from "./user.model";
+
 export class OrderHistory {
+  constructor(
+    public id: number,
+    public firstName: string,
+    public lastName: string,
+    public phone: string,
+    public email: string,
+    public orderedAt: Date,
+    public isCanceled: Boolean,
+    public historyBillingDetail: BillingDetail,
+    public historyTransportDetail: TransportDetail,
+    public ordererUser: User | null,
+    public paymentMethod: PaymentMethod,
+    public status: Status,
+    public cancelerUser: User | null,
+    public orderHistoryProductList: OrderHistoryProduct[]
+  ) { }
 }
