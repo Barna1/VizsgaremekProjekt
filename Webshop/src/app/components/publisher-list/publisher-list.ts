f573a945b8bc74eb1d29d5fedd25972916aad26f
hauszmanndavid <hauszmannd@gmail.com>
2026-02-09 13:59:26 +0100

    publisherList komponens hozzáadása

---
+++ Webshop/src/app/components/publisher-list/publisher-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publisher-list',
+  imports: [],
+  templateUrl: './publisher-list.html',
+  styleUrl: './publisher-list.css',
+})
+export class PublisherList {
+
+}
