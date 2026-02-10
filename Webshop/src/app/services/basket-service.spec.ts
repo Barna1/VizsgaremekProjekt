424ca63d9c2355aef0809ee9224bfa2a8ca07142
hauszmanndavid <hauszmannd@gmail.com>
2026-02-10 13:23:12 +0100

    basketService hozzáadása

---
+++ Webshop/src/app/services/basket-service.spec.ts
@@ -0,0 +1,7 @@
+import { BasketService } from './basket-service';
+
+describe('BasketService', () => {
+  it('should create an instance', () => {
+    expect(new BasketService()).toBeTruthy();
+  });
+});
