68bb744ec8ccd8ac6f72e42e11ec1bd6c622e4e7
hauszmanndavid <hauszmannd@gmail.com>
2026-02-10 11:47:35 +0100

    orderSummaryPage komponens

---
+++ Webshop/src/app/components/order-page/order-summary-page/order-summary-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-summary-page',
+  imports: [],
+  templateUrl: './order-summary-page.html',
+  styleUrl: './order-summary-page.css',
+})
+export class OrderSummaryPage {
+
+}
