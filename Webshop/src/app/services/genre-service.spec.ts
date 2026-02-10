5a6d83c6bab96315bcdec3e23502e8fdb6053ee2
hauszmanndavid <hauszmannd@gmail.com>
2026-02-10 17:40:26 +0100

    genreService hozzáadása

---
+++ Webshop/src/app/services/genre-service.spec.ts
@@ -0,0 +1,7 @@
+import { GenreService } from './genre-service';
+
+describe('GenreService', () => {
+  it('should create an instance', () => {
+    expect(new GenreService()).toBeTruthy();
+  });
+});
