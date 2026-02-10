7bbeb37362120185e2bbee1a15ebd4decc6190be
hauszmanndavid <hauszmannd@gmail.com>
2026-02-10 18:41:21 +0100

    otherService hozzáadása

---
+++ Webshop/src/app/services/other-service.spec.ts
@@ -0,0 +1,7 @@
+import { OtherService } from './other-service';
+
+describe('OtherService', () => {
+  it('should create an instance', () => {
+    expect(new OtherService()).toBeTruthy();
+  });
+});
