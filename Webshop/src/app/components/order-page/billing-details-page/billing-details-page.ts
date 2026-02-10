112af7fa2e8d7604cbadaeb8c4a48ae3122079c3
hauszmanndavid <hauszmannd@gmail.com>
2026-02-10 11:22:28 +0100

    billingDetailsPage komponens hozzáadása

---
+++ Webshop/src/app/components/order-page/billing-details-page/billing-details-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-billing-details-page',
+  imports: [],
+  templateUrl: './billing-details-page.html',
+  styleUrl: './billing-details-page.css',
+})
+export class BillingDetailsPage {
+
+}
