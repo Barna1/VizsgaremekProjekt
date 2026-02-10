478d974a5943ea41ba1ede5b7f5a308761423355
Barna1 <barnabas121744@gmail.com>
2026-02-10 19:48:31 +0100

    JWT alap konfiguráció

---
+++ webshopAPI/src/main/java/com/example/demo/config/security/JWT/JWTProperties.java
@@ -0,0 +1,16 @@
+package com.example.demo.config.security.JWT;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "jwt")
+@Component
+@Getter
+@Setter
+public class JWTProperties {
+    private String secret;
+    private Long expiredTime;
+    private String issuer;
+}
--- webshopAPI/src/main/java/com/example/demo/config/security/JWT/JWTService.java
+++ webshopAPI/src/main/java/com/example/demo/config/security/JWT/JWTService.java
@@ -1,4 +1,75 @@
 package com.example.demo.config.security.JWT;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.demo.config.security.JWT.RefreshToken.RefreshToken;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class JWTService {
+    private final UserRepository userRepository;
+    private static final String AUTH = "auth";
+    private final JWTProperties jwtProperties;
+    private final ObjectMapper mapper;
+
+    public String createJwtToken(UserDetails principal) {
+        System.out.println(principal.getUsername());
+        User loggedUsers = userRepository.findByEmail(principal.getUsername()).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        System.out.println(jwtProperties.getIssuer());
+
+        return JWT.create()
+                .withSubject(loggedUsers.getEmail())
+                .withArrayClaim(AUTH, principal.getAuthorities().stream().map(GrantedAuthority::getAuthority).toArray(String[]::new))
+                .withExpiresAt(new Date(System.currentTimeMillis() + jwtProperties.getExpiredTime()))
+                .withIssuer(jwtProperties.getIssuer())
+                .sign(Algorithm.HMAC256(jwtProperties.getSecret()));
+    }
+
+    public UserDetails parseJwt(String jwtToken) {
+        DecodedJWT jwt = JWT.require(Algorithm.HMAC256(jwtProperties.getSecret())).withIssuer(jwtProperties.getIssuer()).build().verify(jwtToken);
+        return new org.springframework.security.core.userdetails.User(jwt.getSubject(), "dummy", jwt.getClaim(AUTH).asList(String.class).stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+    }
+
+    public String regenerateJwtToken(String refreshTokenValue) {
+        if (refreshTokenValue != null) {
+            byte[] decodedBytes = Base64.getUrlDecoder().decode(refreshTokenValue);
+            String refreshTokenString = new String(decodedBytes, StandardCharsets.UTF_8);
+
+            try {
+                RefreshToken refreshToken = mapper.readValue(refreshTokenString, RefreshToken.class);
+                if (!refreshToken.getExpiredDate().isBefore(Instant.now())) {
+
+                    User loggedUser = userRepository.findByEmail(refreshToken.getEmail()).orElse(null);
+                    if (loggedUser != null && !loggedUser.getIsDeleted()) {
+                        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(loggedUser.getRole().getName()));
+                        String newJwt = createJwtToken(new org.springframework.security.core.userdetails.User(loggedUser.getEmail(), loggedUser.getPassword(), authorities));
+                        return newJwt;
+                    }
+                }
+            } catch (JsonProcessingException e) {
+                return null;
+            }
+        }
+        return null;
+    }
 }
