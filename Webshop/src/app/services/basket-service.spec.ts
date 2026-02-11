46c1263eda5fb5f445d7fe507cfe2cf00bd4fc4e
hauszmanndavid <hauszmannd@gmail.com>
2026-02-11 11:58:21 +0100

    basketService megírása

--- Webshop/src/app/services/basket-service.spec.ts
+++ Webshop/src/app/services/basket-service.spec.ts
@@ -1,7 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
 import { BasketService } from './basket-service';
 
 describe('BasketService', () => {
-  it('should create an instance', () => {
-    expect(new BasketService()).toBeTruthy();
+  let service: BasketService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(BasketService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
 });
--- Webshop/src/app/services/basket-service.ts
+++ Webshop/src/app/services/basket-service.ts
@@ -1,2 +1,35 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Basket } from "../models/basket.model";
+import { Observable } from "rxjs";
+import { BasketProduct } from "../models/basket-product.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+
 export class BasketService {
+    private baseUrl = "http://localhost:8080/basket"
+    private http = inject(HttpClient)
+    offlineBasket: Basket = new Basket(null, 0, [])
+
+    getBasketByUserId(userId: number): Observable<Basket> {
+        return this.http.get<Basket>(`${this.baseUrl}/user/${userId}`)
+    }
+
+    deleteProduct(basketProductId: number, basketId: number) {
+        return this.http.delete(`${this.baseUrl}/book?basketProductId=${basketProductId}&basketId=${basketId}`)
+    }
+
+    changeAmountOfProduct(basketId:number, body: {productId:number, amount:number}): Observable<BasketProduct> {
+    return this.http.patch<BasketProduct>(`${this.baseUrl}/${basketId}`, body)
+  }
+
+  addProductToBasket(basketId:number, body: {productId: number, amount: number}) {
+    return this.http.post(`${this.baseUrl}/${basketId}`, body)
+  }
+
+  clearBasket(basketId: number) {
+    return this.http.delete(`${this.baseUrl}/${basketId}/clear`)
+  }
 }
