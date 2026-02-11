1fbc8c1c2c96fa12aee956928c5305f4453325ad
hauszmanndavid <hauszmannd@gmail.com>
2026-02-11 09:37:17 +0100

    publisherService hozzáadása

---
+++ Webshop/src/app/services/publisher-service.spec.ts
@@ -0,0 +1,7 @@
+import { PublisherService } from './publisher-service';
+
+describe('PublisherService', () => {
+  it('should create an instance', () => {
+    expect(new PublisherService()).toBeTruthy();
+  });
+});
