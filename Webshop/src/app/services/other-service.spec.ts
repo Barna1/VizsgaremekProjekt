9495f0215b1ece05b8242e719660efbcb16cf33e
hauszmanndavid <hauszmannd@gmail.com>
2026-02-11 13:12:09 +0100

    otherService megírása

--- Webshop/src/app/services/other-service.spec.ts
+++ Webshop/src/app/services/other-service.spec.ts
@@ -1,7 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
 import { OtherService } from './other-service';
 
 describe('OtherService', () => {
-  it('should create an instance', () => {
-    expect(new OtherService()).toBeTruthy();
+  let service: OtherService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(OtherService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
 });
--- Webshop/src/app/services/other-service.ts
+++ Webshop/src/app/services/other-service.ts
@@ -1,2 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { PaymentMethod } from "../models/payment-method.model";
+import { AddressType } from "../models/address-type.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+
 export class OtherService {
+    private baseUrl:string = "http://localhost:8080"
+    private http = inject(HttpClient)
+
+    getAllPaymentMethod(): Observable<PaymentMethod[]> {
+        return this.http.get<PaymentMethod[]>(`${this.baseUrl}/paymentMethods`)
+    }
+
+    getAllAddressType(): Observable<AddressType[]> {
+        return this.http.get<AddressType[]>(`${this.baseUrl}/addressType`)
+    }
 }
