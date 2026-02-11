5f8c3e314fc4972f144b3a84be74eb6397429d4f
hauszmanndavid <hauszmannd@gmail.com>
2026-02-11 12:49:20 +0100

    orderService megírása

--- Webshop/src/app/services/order-service.spec.ts
+++ Webshop/src/app/services/order-service.spec.ts
@@ -1,7 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
 import { OrderService } from './order-service';
 
 describe('OrderService', () => {
-  it('should create an instance', () => {
-    expect(new OrderService()).toBeTruthy();
+  let service: OrderService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(OrderService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
 });
--- Webshop/src/app/services/order-service.ts
+++ Webshop/src/app/services/order-service.ts
@@ -1,2 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OrderHistory } from '../models/order-history.model';
+
+@Injectable({
+  providedIn: 'root',
+})
 export class OrderService {
+  private baseUrl = 'http://localhost:8080/order';
+  private http = inject(HttpClient);
+
+  getOrderHistoryById(userId: number): Observable<OrderHistory[]> {
+    return this.http.get<OrderHistory[]>(`${this.baseUrl}/user/${userId}`);
+  }
+
+  cancelOrder(orderId: number, userId: number | null) {
+    return this.http.delete('');
+  }
+
+  getOrderHistoryByVCode(email: string, vCode: string): Observable<OrderHistory> {
+    return this.http.get<OrderHistory>('');
+  }
+
+  getAllOrder(): Observable<OrderHistory[]> {
+    return this.http.get<OrderHistory[]>(`${this.baseUrl}`);
+  }
+
+  sendOrder(newOrder: OrderHistory, basketId: number) {
+    return this.http.post('', newOrder);
+  }
 }
