514e5281cce81b0cddac35f109cfdf91a8a65582
hauszmanndavid <hauszmannd@gmail.com>
2026-02-12 13:11:06 +0100

    register.ts megírása

--- Webshop/src/app/app.config.ts
+++ Webshop/src/app/app.config.ts
@@ -8,7 +8,6 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch())
   ]
--- Webshop/src/app/components/register/register.ts
+++ Webshop/src/app/components/register/register.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../services/user-service';
+import { User } from '../../models/user.model';
+import { Router, RouterModule } from '@angular/router';
+
+function validatePassword(control: AbstractControl): { [key: string]: any } | null {
+  const password: string = control.value
+
+  const specialCharacters: string = "!@#$%^&*()-_=+[]{};:,.?/"
+  const numberTexts: string = "1234567890"
+  const checkerList: boolean[] = [false, false, false, false]
+
+  for (let i: number = 0; i < password.length; i++) {
+    if (specialCharacters.includes(password[i])) {
+      checkerList[0] = true
+    } else if (numberTexts.includes(password[i])) {
+      checkerList[1] = true
+    } else if (password[i] === password[i].toUpperCase()) {
+      checkerList[2] = true
+    } else if (password[i] === password[i].toLowerCase()) {
+      checkerList[3] = true
+    }
+  }
+
+  if (!checkerList.includes(false)) {
+    return null
+  } else {
+    return { invalid: false }
+  }
+}
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {
+export class Register implements OnInit {
+  registerForm!: FormGroup;
+  private userService = inject(UserService)
+  private router = inject(Router)
+  errorMsg: string | null = null
 
+  samePasswordValidator = (control: AbstractControl): { [key: string]: any } | null => {
+    let originalPassword = this.registerForm.controls["password"].value
+    if (control.value === originalPassword) {
+      return null
+    } else {
+      return { invalid: false }
+    }
+  }
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl("", [Validators.required]),
+      email: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required, validatePassword]),
+      passwordAgain: new FormControl("", [Validators.required, validatePassword])
+    })
+
+    this.registerForm.controls["passwordAgain"].addValidators(this.samePasswordValidator)
+  }
+
+  sendRegister() {
+    const newUser: User = new User(null, this.registerForm.controls["username"].value, this.registerForm.controls["email"].value, this.registerForm.controls["password"].value)
+    this.userService.register(newUser).subscribe({
+      next: response => {
+        console.log(response)
+      },
+      error: error => console.log(error),
+      complete: () => {
+        this.router.navigate(["/login"])
+      }
+    })
+  }
 }
