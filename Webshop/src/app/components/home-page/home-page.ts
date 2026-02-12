7eba6ab09f3481bd300d114f60365566bff81147
hauszmanndavid <hauszmannd@gmail.com>
2026-02-12 10:58:22 +0100

    homePage.ts megírása

--- Webshop/src/app/components/home-page/home-page.ts
+++ Webshop/src/app/components/home-page/home-page.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { BookService } from './../../services/book-service';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductCard } from '../product-card/product-card';
+import { Book } from '../../models/book.model';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  bookService = inject(BookService)
+  successfullyBooks: Book[] = []
 
+
+  ngOnInit(): void {
+      this.bookService.getMostSuccessFullyBooks().subscribe({
+        next: response => {
+          console.log(response);
+          this.successfullyBooks = response
+        }
+      })
+  }
 }
