4c35b10d8f128c309417e1ab6cef61ae98fdfa68
hauszmanndavid <hauszmannd@gmail.com>
2026-02-12 12:07:04 +0100

    navbar.ts megírása

--- Webshop/src/app/components/navbar/navbar.ts
+++ Webshop/src/app/components/navbar/navbar.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { UserService } from './../../services/user-service';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  router = inject(Router)
+  userService = inject(UserService)
 
+  navigateUserButton() {
+    if(this.userService.loggedUser == null) {
+      this.router.navigate(["/login"])
+    } else {
+      this.router.navigate(["/userPage"])
+    }
+  }
 }
