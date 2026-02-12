b8a781905f7202e0926d01e10ee2bfbf49d7fa89
hauszmanndavid <hauszmannd@gmail.com>
2026-02-12 17:48:30 +0100

    productCard.ts megírása

--- Webshop/src/app/components/product-card/product-card.ts
+++ Webshop/src/app/components/product-card/product-card.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { Book } from '../../models/book.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
@@ -7,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './product-card.css',
 })
 export class ProductCard {
+  private router = inject(Router)
+  productDetail = input.required<Book>()
 
+  navigate() {
+    console.log(this.productDetail())
+    this.router.navigate(["productDetail", this.productDetail()?.id])
+  }
 }
