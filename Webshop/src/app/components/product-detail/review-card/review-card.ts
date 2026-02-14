35e3db91276253a655753007896d08dff613f275
hauszmanndavid <hauszmannd@gmail.com>
2026-02-14 17:46:01 +0100

    reviewCard hozzáadása

---
+++ Webshop/src/app/components/product-detail/review-card/review-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-card',
+  imports: [],
+  templateUrl: './review-card.html',
+  styleUrl: './review-card.css',
+})
+export class ReviewCard {
+
+}
