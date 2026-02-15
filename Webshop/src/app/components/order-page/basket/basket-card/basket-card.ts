6a2ae84424930fb8f1d2f5a4ac482d5d27ea8924
hauszmanndavid <hauszmannd@gmail.com>
2026-02-15 14:10:45 +0100

    basket-card.ts megírása

--- Webshop/src/app/components/order-page/basket/basket-card/basket-card.ts
+++ Webshop/src/app/components/order-page/basket/basket-card/basket-card.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Book } from '../../../../models/book.model';
+import { BasketService } from '../../../../services/basket-service';
+import { BasketProduct } from '../../../../models/basket-product.model';
 
 @Component({
   selector: 'app-basket-card',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './basket-card.html',
   styleUrl: './basket-card.css',
 })
 export class BasketCard {
+  book = input.required<BasketProduct>()
+  basketId = input.required<number>()
+  basketService = inject(BasketService)
+  delete = output<number>()
+  changeAmount = output<number>()
 
+  changeAmountOfProduct(plusValue: 1 | -1) {
+    this.basketService.changeAmountOfProduct(this.basketId(), {productId: this.book().id, amount: this.book().amount + plusValue}).subscribe({
+      next: response => {
+        this.changeAmount.emit(this.book().amount + plusValue)
+      }
+    })
+  }
+
+  deleteProductFromBasket(id: number) {
+    this.basketService.deleteProduct(id, this.basketId()).subscribe({
+      next: response => {
+        this.delete.emit(id)
+      }
+    })
+  }
 }
