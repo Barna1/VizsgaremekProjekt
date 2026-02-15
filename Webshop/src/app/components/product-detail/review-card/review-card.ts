40c8647754eebbf1658e14caa454a9e55440fefb
hauszmanndavid <hauszmannd@gmail.com>
2026-02-15 09:29:38 +0100

    review-card.ts megírása

--- Webshop/src/app/components/product-detail/review-card/review-card.ts
+++ Webshop/src/app/components/product-detail/review-card/review-card.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Review } from '../../../models/review.model';
 
 @Component({
   selector: 'app-review-card',
   imports: [],
   templateUrl: './review-card.html',
-  styleUrl: './review-card.css',
+  styleUrl: './review-card.scss',
 })
 export class ReviewCard {
+  reviewDetail = input.required<Review>()
+
 
 }
