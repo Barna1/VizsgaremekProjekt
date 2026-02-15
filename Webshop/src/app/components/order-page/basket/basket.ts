32145193414133be9b3ab02c496c82e5620d92c2
hauszmanndavid <hauszmannd@gmail.com>
2026-02-15 11:22:44 +0100

    basket.ts megírása

--- Webshop/src/app/components/order-page/basket/basket.ts
+++ Webshop/src/app/components/order-page/basket/basket.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { BasketCard } from './basket-card/basket-card';
+import { BasketService } from '../../../services/basket-service';
+import { UserService } from '../../../services/user-service';
+import { Basket } from '../../../models/basket.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-basket',
-  imports: [],
+  imports: [BasketCard, RouterModule],
   templateUrl: './basket.html',
   styleUrl: './basket.css',
 })
-export class Basket {
+export class BasketPage implements OnInit {
+  basketService = inject(BasketService)
+  userService = inject(UserService)
+  basket!: Basket
 
+  ngOnInit(): void {
+    this.basketService.getBasketByUserId(11).subscribe({
+      next: response => this.basket = response,
+    })
+  }
+
+  changeAmount(newAmount: number) {
+
+  }
+
+  deleteProductFromBasket(id: number) {
+
+  }
 }
