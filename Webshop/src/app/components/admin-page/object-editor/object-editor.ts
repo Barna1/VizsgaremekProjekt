3f92891e1cbebfc737f1aa0fb10c98ccf9356593
hauszmanndavid <hauszmannd@gmail.com>
2026-02-16 16:34:52 +0100

    object-editor hozzáadása

---
+++ Webshop/src/app/components/admin-page/object-editor/object-editor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-object-editor',
+  imports: [],
+  templateUrl: './object-editor.html',
+  styleUrl: './object-editor.css',
+})
+export class ObjectEditor {
+
+}
