e05e30a931e592a9a525b913245454b94f435eb4
hauszmanndavid <hauszmannd@gmail.com>
2026-02-16 16:52:28 +0100

    order-history-page hozzáadása

---
+++ Webshop/src/app/components/admin-page/order-history-page/order-history-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-history-page',
+  imports: [],
+  templateUrl: './order-history-page.html',
+  styleUrl: './order-history-page.css',
+})
+export class OrderHistoryPage {
+
+}
