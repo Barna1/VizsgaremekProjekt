e4e1919c7dfd2ef0bc0e308aab3206e006d1a0e0
hauszmanndavid <hauszmannd@gmail.com>
2026-02-16 10:20:07 +0100

    order-history-details-pop-up.ts megírása

--- Webshop/src/app/components/order-history-details-pop-up/order-history-details-pop-up.ts
+++ Webshop/src/app/components/order-history-details-pop-up/order-history-details-pop-up.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { OrderHistory } from '../../models/order-history.model';
 
 @Component({
   selector: 'app-order-history-details-pop-up',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './order-history-details-pop-up.css',
 })
 export class OrderHistoryDetailsPopUp {
-
+  historyDetail = input.required<OrderHistory>()
 }
