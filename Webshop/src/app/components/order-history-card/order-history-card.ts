0817e7f2e0ac797f8e889f050aea079cc79c0d88
hauszmanndavid <hauszmannd@gmail.com>
2026-02-16 09:22:27 +0100

    order-history-card.ts megírása

--- Webshop/src/app/components/order-history-card/order-history-card.ts
+++ Webshop/src/app/components/order-history-card/order-history-card.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit, output } from '@angular/core';
+import { OrderHistory } from '../../models/order-history.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-order-history-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './order-history-card.html',
   styleUrl: './order-history-card.css',
 })
-export class OrderHistoryCard {
+export class OrderHistoryCard implements OnInit{
+  historyDetails = input.required<OrderHistory>()
+  sumPrice: number = 0
+  showDetails = output<OrderHistory>()
 
+  ngOnInit(): void {
+    for (let i: number = 0; i <this.historyDetails().orderHistoryProductList.length; i++) {
+      this.sumPrice += (this.historyDetails().orderHistoryProductList[i].orderHistoryBook.price * this.historyDetails().orderHistoryProductList[i].amount)
+    }
+  }
 }
