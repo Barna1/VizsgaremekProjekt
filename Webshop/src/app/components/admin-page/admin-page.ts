a1bfb2f402059613ffc16b23d5ade01e628ab8dd
hauszmanndavid <hauszmannd@gmail.com>
2026-02-16 15:21:07 +0100

    admin-page.ts megírása

--- Webshop/src/app/components/admin-page/admin-page.ts
+++ Webshop/src/app/components/admin-page/admin-page.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-page',
-  imports: [],
+  imports: [RouterModule, ],
   templateUrl: './admin-page.html',
   styleUrl: './admin-page.css',
 })
-export class AdminPage {
+export class AdminPage implements OnInit {
+  isShowPopUp: boolean = false
+  selectedType!: "genre" | "book" | "publisher"
 
+  ngOnInit(): void {
+
+  }
+
+  showPopup(selectedType: "genre" | "book" | "publisher") {
+    this.selectedType = selectedType
+    this.isShowPopUp = true
+  }
 }
