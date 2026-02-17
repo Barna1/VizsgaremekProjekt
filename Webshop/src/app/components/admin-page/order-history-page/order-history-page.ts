be52ef6d2e7cf766e0d38f3db59507b1c8e2b2dc
hauszmanndavid <hauszmannd@gmail.com>
2026-02-17 10:19:36 +0100

    order-history-page.ts megírása

--- Webshop/src/app/components/admin-page/order-history-page/order-history-page.ts
+++ Webshop/src/app/components/admin-page/order-history-page/order-history-page.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../../../services/order-service';
+import { OrderHistory } from '../../../models/order-history.model';
+import { OrderHistoryDetailsPopUp } from '../../order-history-details-pop-up/order-history-details-pop-up';
+import { OrderHistoryCard } from '../../order-history-card/order-history-card';
 
 @Component({
   selector: 'app-order-history-page',
-  imports: [],
+  imports: [OrderHistoryDetailsPopUp, OrderHistoryCard],
   templateUrl: './order-history-page.html',
   styleUrl: './order-history-page.css',
 })
-export class OrderHistoryPage {
+export class OrderHistoryPage implements OnInit {
+  orderService = inject(OrderService)
+  selectedOrderHistory!: OrderHistory
+  orderHistories: OrderHistory[] = []
 
+  ngOnInit(): void {
+    this.orderService.getAllOrder().subscribe({
+      next: response => this.orderHistories = response
+    })
+  }
 }
