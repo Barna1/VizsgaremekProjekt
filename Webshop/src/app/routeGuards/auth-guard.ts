7d4c7fd17af450561099aee1476aa346c5339be9
hauszmanndavid <hauszmannd@gmail.com>
2026-02-18 10:15:13 +0100

    auth-guard megírása

--- Webshop/src/app/routeGuards/auth-guard.ts
+++ Webshop/src/app/routeGuards/auth-guard.ts
@@ -1,2 +1,20 @@
-export class AuthGuard {
+import { UserService } from './../services/user-service';
+import { inject, Injectable } from "@angular/core";
+import { CanMatch, RedirectCommand, Route, Router, UrlSegment } from "@angular/router";
+
+@Injectable({
+    providedIn: "root"
+})
+
+export class AuthGuard implements CanMatch {
+    userService = inject(UserService)
+    router = inject(Router)
+
+    canMatch(route: Route, segments: UrlSegment[]) {
+        if (this.userService.loggedUser != null) {
+            return true
+        }
+
+        return new RedirectCommand(this.router.parseUrl("/unauthorized"))
+    }
 }
