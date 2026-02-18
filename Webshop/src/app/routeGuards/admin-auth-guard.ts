d5b5bea6da5d8591234e779505919d77bb237f63
hauszmanndavid <hauszmannd@gmail.com>
2026-02-18 10:30:59 +0100

    admin-auth-guard megírása

--- Webshop/src/app/routeGuards/admin-auth-guard.ts
+++ Webshop/src/app/routeGuards/admin-auth-guard.ts
@@ -1,2 +1,20 @@
-export class AdminAuthGuard {
+import { inject, Injectable } from "@angular/core";
+import { CanMatch, GuardResult, MaybeAsync, RedirectCommand, Route, Router, UrlSegment } from "@angular/router";
+import { UserService } from "../services/user-service";
+
+@Injectable({
+    providedIn: "root"
+})
+
+export class AdminAuthGuard implements CanMatch {
+    userService = inject(UserService)
+    router = inject(Router)
+
+    canMatch(route: Route, segments: UrlSegment[]) {
+        if (this.userService.loggedUser?.role?.name == "ROLE_admin") {
+            return true
+        }
+
+        return new RedirectCommand(this.router.parseUrl("/unauthorized"))
+    }
 }
