08edaf06e1c707d0a72c5e1c62d48ee0b661d198
hauszmanndavid <hauszmannd@gmail.com>
2026-02-19 11:59:04 +0100

    list-card.ts megcsinálása

--- Webshop/src/app/components/admin-page/popup/list-card/list-card.ts
+++ Webshop/src/app/components/admin-page/popup/list-card/list-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-list-card',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './list-card.css',
 })
 export class ListCard {
-
+  card = input.required<{id: number, name: string}>()
+  edit = output()
+  delete = output()
 }
