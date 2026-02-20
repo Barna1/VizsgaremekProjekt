6f6eed55b9bb12626ff61b5aab7faf624e90712c
hauszmanndavid <hauszmannd@gmail.com>
2026-02-20 14:07:23 +0100

    billing-details-page modosítása

--- Webshop/src/app/components/order-page/billing-details-page/billing-details-page.ts
+++ Webshop/src/app/components/order-page/billing-details-page/billing-details-page.ts
@@ -5,6 +5,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { AddressType } from '../../../models/address-type.model';
 import { PaymentMethod } from '../../../models/payment-method.model';
 import { Router, RouterModule } from '@angular/router';
+import { BillingDetail } from '../../../models/billing-detail.model';
 
 @Component({
   selector: 'app-billing-details-page',
@@ -31,20 +32,49 @@ export class BillingDetailsPage implements OnInit{
     })
 
     this.form = new FormGroup({
-      postCode: new FormControl("", [Validators.required]),
-      town: new FormControl("", [Validators.required]),
-      addressType: new FormControl("", [Validators.required]),
-      address: new FormControl("", [Validators.required]),
-      houseNumber: new FormControl("", [Validators.required]),
-      other: new FormControl("", []),
-      companyName: new FormControl("", []),
-      taxNumber: new FormControl("", []),
-      paymentMethod: new FormControl("", [Validators.required])
+      postCode: new FormControl(this.orderService.actualOrder.historyBillingDetail?.postCode, [Validators.required]),
+      town: new FormControl(this.orderService.actualOrder.historyBillingDetail?.town, [Validators.required]),
+      addressType: new FormControl(this.orderService.actualOrder.historyBillingDetail?.billingDetailsAddressType.id, [Validators.required]),
+      address: new FormControl(this.orderService.actualOrder.historyBillingDetail?.address, [Validators.required]),
+      houseNumber: new FormControl(this.orderService.actualOrder.historyBillingDetail?.houseNumber, [Validators.required]),
+      other: new FormControl(this.orderService.actualOrder.historyBillingDetail?.other, []),
+      // companyName: new FormControl(this.orderService.actualOrder.historyBillingDetail?.companyName, []),
+      // taxNumber: new FormControl(this.orderService.actualOrder.historyBillingDetail?.companyTaxNumber, []),
+      paymentMethod: new FormControl(this.orderService.actualOrder.paymentMethod?.id, [Validators.required])
     })
   }
 
   continue() {
+    const billingDetail: BillingDetail = new BillingDetail(
+      null,
+      this.form.controls["postCode"].value,
+      this.form.controls["town"].value,
+      this.form.controls["address"].value,
+      this.form.controls["houseNumber"].value,
+      this.isCompany ? this.form.controls["companyName"].value : null,
+      this.isCompany ? this.form.controls["taxNumber"].value : null,
+      this.form.controls["other"].value,
+      this.addressTypes[this.addressTypes.findIndex(at => at.id === this.form.controls["addressType"].value)],
+    )
 
+    this.orderService.actualOrder.historyBillingDetail = billingDetail
+    this.orderService.actualOrder.paymentMethod = this.paymentMethods[this.paymentMethods.findIndex(pm => pm.id === this.form.controls["paymentMethod"].value)]
     this.router.navigate(["orderPage", "summary"])
+  }
+
+  setPaymentMethod(selectedId: number) {
+    this.form.controls["paymentMethod"].setValue(selectedId)
+  }
+
+  handleCompany() {
+    if (this.isCompany) {
+      this.form.removeControl("companyName")
+      this.form.removeControl("taxNumber")
+    } else {
+      this.form.addControl("companyName", new FormControl(this.orderService.actualOrder.historyBillingDetail?.companyName, [Validators.required]))
+      this.form.addControl("taxNumber", new FormControl(this.orderService.actualOrder.historyBillingDetail?.companyTaxNumber, [Validators.required]))
+    }
+
+    this.isCompany = !this.isCompany
   }
 }
