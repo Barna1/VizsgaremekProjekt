ee78ab45ed354da904058d476e14be85080f195d
hauszmanndavid <hauszmannd@gmail.com>
2026-02-21 17:59:08 +0100

    publisher-list módosítása

--- Webshop/src/app/components/publisher-list/publisher-list.ts
+++ Webshop/src/app/components/publisher-list/publisher-list.ts
@@ -1,16 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PublisherService } from '../../services/publisher-service';
 import { Publisher } from '../../models/publisher.model';
+import { BookService } from '../../services/book-service';
+import { Book } from '../../models/book.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-publisher-list',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './publisher-list.html',
   styleUrl: './publisher-list.css',
 })
 export class PublisherList implements OnInit{
   publisherService = inject(PublisherService)
+  bookService = inject(BookService)
   publishers: Publisher[] = []
+  selectedPublisher: Publisher | null = null
+  bookList: Book[] = []
 
   ngOnInit(): void {
     this.publisherService.getAllPublisher().subscribe({
@@ -18,4 +24,10 @@ export class PublisherList implements OnInit{
     })
   }
 
+  selectPublisher(selectedPublisher: Publisher) {
+    this.selectedPublisher = selectedPublisher
+    this.bookService.getBooksByPublisher(selectedPublisher.id).subscribe({
+      next: response => this.bookList = response
+    })
+  }
 }
