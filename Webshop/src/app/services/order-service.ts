ae55d8540a0f245d885c7b00b155e6af58e4ea44
hauszmanndavid <hauszmannd@gmail.com>
2026-03-01 15:20:42 +0100

    transport-details reszponzivitása

--- Webshop/src/app/services/order-service.ts
+++ Webshop/src/app/services/order-service.ts
@@ -1,30 +1,26 @@
+import { OrderHistory } from './../models/order-history.model';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { OrderHistory } from '../models/order-history.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderService {
-  baseUrl = 'http://localhost:8080/order'
   http = inject(HttpClient)
-  actualOrder: OrderHistory = new OrderHistory() 
+  baseUrl = "http://localhost:8080/order"
+  actualOrder: OrderHistory = new OrderHistory()
 
-  getOrderHistoryById(userId: number): Observable<OrderHistory[]> {
-    return this.http.get<OrderHistory[]>(`${this.baseUrl}/user/${userId}`);
+  getOrderHistoryByUserId(userId: number): Observable<OrderHistory[]> {
+    return this.http.get<OrderHistory[]>(`${this.baseUrl}/user/${userId}`)
   }
 
-  cancelOrder(orderId: number, userId: number | null) {
-    return this.http.delete('');
-  }
-
-  getOrderHistoryByVCode(email: string, vCode: string): Observable<OrderHistory> {
-    return this.http.get<OrderHistory>('');
+  cancelOrder(orderId: number, userId: number | null): Observable<OrderHistory> {
+    return this.http.delete<OrderHistory>(`${this.baseUrl}/cancel?orderId=${orderId}&userId=${userId}`)
   }
 
   getAllOrder(): Observable<OrderHistory[]> {
-    return this.http.get<OrderHistory[]>(`${this.baseUrl}`);
+    return this.http.get<OrderHistory[]>(`${this.baseUrl}`)
   }
 
   sendOrder(basketId: number) {
