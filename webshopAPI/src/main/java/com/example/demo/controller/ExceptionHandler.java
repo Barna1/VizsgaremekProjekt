a74971174b50e373113f9ff41bc26339f72abe34
Barna1 <barnabas121744@gmail.com>
2026-03-02 10:14:01 +0100

    ExceptionHandler megírása

---
+++ webshopAPI/src/main/java/com/example/demo/controller/ExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        System.out.println(ex.getMessage());
+        if (ex.getMessage().contains("for key 'phone'")) {
+            return ResponseEntity.status(409).body(Map.of("statusText", "duplicatePhone"));
+        } else if (ex.getMessage().contains("for key 'email'")) {
+            return ResponseEntity.status(409).body(Map.of("statusText", "duplicateEmail"));
+        }
+
+        return ResponseEntity.internalServerError().build();
+    }
+}
