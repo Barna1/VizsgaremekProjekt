6abfc1085eb843a0b66f342a0fcd726156370940
hauszmanndavid <hauszmannd@gmail.com>
2026-03-17 13:56:14 +0100

    alert classok hozzáadása

--- Webshop/src/app/components/product-detail/product-detail.ts
+++ Webshop/src/app/components/product-detail/product-detail.ts
@@ -5,10 +5,11 @@ import { Book } from '../../models/book.model';
 import { BookService } from '../../services/book-service';
 import { ReviewCard } from './review-card/review-card';
 import { BasketService } from '../../services/basket-service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [ReviewCard],
+  imports: [ReviewCard, NgClass],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
 })
@@ -18,6 +19,9 @@ export class ProductDetail implements OnInit{
   private userService = inject(UserService)
   private route = inject(ActivatedRoute)
   selectedBook!: Book
+  showAlert = false;
+  type: 'success' | 'danger' = 'success';
+  message = '';
 
 
   ngOnInit(): void {
@@ -39,9 +43,22 @@ export class ProductDetail implements OnInit{
   addToCart() {
     console.log("Add to Cart")
       this.basketService.addProductToBasket(this.userService.loggedUser?.id!, {productId: this.selectedBook.id!, amount: 1}).subscribe({
-        next: response => console.log(response)
-      })
 
+        next: res => {
+      this.type = 'success';
+      this.message = 'Termék hozzáadva a kosárhoz!';
+      this.showAlert = true;
+
+    },
+
+    error: err => {
+      this.type = 'danger';
+      this.message = 'Nem sikerült kosárba rakni!';
+      this.showAlert = true;
+    }
+
+      });
+      }
+      
+      
   }
-
-}
