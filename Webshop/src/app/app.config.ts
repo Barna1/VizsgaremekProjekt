a7413796ede4a0101486474054e2d0827030a80d
hauszmanndavid <hauszmannd@gmail.com>
2026-03-24 10:01:58 +0100

    frissítés

--- Webshop/src/app/app.config.ts
+++ Webshop/src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { AuthInterceptor } from './interceptor/auth-interceptor';
+import { CookieSetterInterceptor } from './interceptor/cookie-setter-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
