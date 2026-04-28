9ac9cc1aaab02cb8dcf936979251f56719bdc8ae
hauszmanndavid <hauszmannd@gmail.com>
2026-04-28 13:40:41 +0200

    dizájnolás

--- Webshop/src/app/components/order-page/basket/basket-card/basket-card.ts
+++ Webshop/src/app/components/order-page/basket/basket-card/basket-card.ts
@@ -13,11 +13,11 @@ import { UserService } from '../../../../services/user-service';
 export class BasketCard {
   book = input.required<BasketProduct>()
   basketService = inject(BasketService)
-  usreService = inject(UserService)
+  userService = inject(UserService)
   changeAmount = output<number>()
 
   changeAmountOfProduct(plusValue: 1 | -1) {
-    this.basketService.changeAmountOfProduct(this.usreService.loggedUser?.id!, {productId: this.book().id, newAmount: this.book().amount + plusValue}).subscribe({
+    this.basketService.changeAmountOfProduct(this.userService.loggedUser?.id!, {productId: this.book().id, newAmount: this.book().amount + plusValue}).subscribe({
       next: response => {
         if (this.book().amount + plusValue === 0) {
           this.basketService.usersBasket.productList = this.basketService.usersBasket.productList?.filter((bp) => bp.id != this.book().id)
@@ -29,7 +29,7 @@ export class BasketCard {
   }
 
   deleteProductFromBasket() {
-    this.basketService.deleteProduct(this.book().id, this.usreService.loggedUser?.id!).subscribe({
+    this.basketService.deleteProduct(this.book().id, this.userService.loggedUser?.id!).subscribe({
       next: response => {
         this.basketService.usersBasket.productList = this.basketService.usersBasket.productList?.filter((bp) => bp.id != this.book().id)
       }
