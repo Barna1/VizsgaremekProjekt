3ed39b595ea635ee315c3d3ff4720508dcfa2df2
hauszmanndavid <hauszmannd@gmail.com>
2026-04-28 19:20:57 +0200

    keresés hozzáadása

--- Webshop/src/app/components/product-list/product-list.ts
+++ Webshop/src/app/components/product-list/product-list.ts
@@ -11,81 +11,96 @@ import { GenreService } from '../../services/genre-service';
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
-export class ProductList implements OnInit{
-  bookService = inject(BookService)
-  genreService = inject(GenreService)
-  bookList: Book[] = []
-  genreList: Genre[] = []
+export class ProductList implements OnInit {
+  bookService = inject(BookService);
+  genreService = inject(GenreService);
+  bookList: Book[] = [];
+  genreList: Genre[] = [];
   pageNumber = 0;
-  isAsc: boolean = false
-  sortType: string = "id"
-  isError: boolean = false
-  availablePages: number[] = []
-  selectedGenre: null | Genre = null
+  isAsc: boolean = false;
+  sortType: string = 'id';
+  isError: boolean = false;
+  availablePages: number[] = [];
+  selectedGenre: null | Genre = null;
+  selectedGenreId: number | null = null;
 
   ngOnInit(): void {
-    this.getBookPage()
+    this.getBookPage();
     this.genreService.getAllGenre().subscribe({
-      next: response => this.genreList = response,
-    })
+      next: (response) => (this.genreList = response),
+    });
+  }
+
+  toggleGenre(id: number) {
+    if (this.selectedGenreId === id) {
+      this.selectedGenreId = null; // kikapcsolás
+    } else {
+      this.selectedGenreId = id; // kiválasztás
+    }
   }
 
   getBookPage() {
     this.bookService.getBooksPage(this.pageNumber, this.sortType, this.isAsc).subscribe({
-      next: response => {
-        this.bookList = response.body as Book[]
-        const pageNumber: number = +response.headers.get("totalpage")!
-        this.availablePages = Array(pageNumber).fill(1).map((x,i)=>i+1)
+      next: (response) => {
+        this.bookList = response.body as Book[];
+        const pageNumber: number = +response.headers.get('totalpage')!;
+        this.availablePages = Array(pageNumber)
+          .fill(1)
+          .map((x, i) => i + 1);
       },
-      error: error => this.isError = true
-    })
+      error: (error) => (this.isError = true),
+    });
   }
 
   getRows(): Book[][] {
-    const rows: Book[][] = []
-    for (let i: number = 0; i< 12; i+=3) {
-      const row: Book[] = []
-      for (let j = i; j < i+3; j++) {
+    const rows: Book[][] = [];
+    for (let i: number = 0; i < 12; i += 3) {
+      const row: Book[] = [];
+      for (let j = i; j < i + 3; j++) {
         if (this.bookList[j] != undefined) {
-          row.push(this.bookList[j])
+          row.push(this.bookList[j]);
         }
       }
-      rows.push(row)
+      rows.push(row);
     }
 
     return rows;
   }
 
   changePageWithArrows(isForward: boolean) {
-    if ((this.pageNumber - 1 >= 0 && !isForward) || (this.pageNumber + 1 < this.availablePages.length && isForward)) {
+    if (
+      (this.pageNumber - 1 >= 0 && !isForward) ||
+      (this.pageNumber + 1 < this.availablePages.length && isForward)
+    ) {
       this.pageNumber += isForward ? 1 : -1;
       if (this.selectGenre == null) {
-        this.getBookPage()
+        this.getBookPage();
       } else {
-
       }
     }
   }
 
   selectGenre(genre: Genre) {
     if (this.selectedGenre == null) {
-      this.getBooksByGenre(genre.id!)
+      this.getBooksByGenre(genre.id!);
       this.selectedGenre = genre;
-      this.getBooksByGenre(genre.id!)
+      this.getBooksByGenre(genre.id!);
     } else {
-      this.selectedGenre = null
-      this.pageNumber = 0
-      this.getBookPage()
+      this.selectedGenre = null;
+      this.pageNumber = 0;
+      this.getBookPage();
     }
   }
 
   getBooksByGenre(id: number) {
-    this.bookService.getBookByGenre(id, this.pageNumber, "id", true).subscribe({
-      next: response => {
-        this.bookList = response.body as Book[]
-        const pageNumber: number = +response.headers.get("totalpage")!
-        this.availablePages = Array(pageNumber).fill(1).map((x,i)=>i+1)
-      }
-    })
+    this.bookService.getBookByGenre(id, this.pageNumber, 'id', true).subscribe({
+      next: (response) => {
+        this.bookList = response.body as Book[];
+        const pageNumber: number = +response.headers.get('totalpage')!;
+        this.availablePages = Array(pageNumber)
+          .fill(1)
+          .map((x, i) => i + 1);
+      },
+    });
   }
 }
